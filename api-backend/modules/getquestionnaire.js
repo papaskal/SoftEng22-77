8b050865db0f4b562235c75a7eab1274cd03c11b
Alexandros Papaskarlatos <el11097@mail.ntua.gr>
2023-01-16 05:40:51 +0200

    Add error handling for getquestionnaire

--- api-backend/modules/getquestionnaire.js
+++ api-backend/modules/getquestionnaire.js
@@ -2,20 +2,9 @@ const mongoose = require('mongoose')
 const Questionnaire = require('../models/questionnaire')
 
 
-// const getQuestionnaire = async (questionnaireID) => {
-//     const questionnaire = await Questionnaire.find({questionnaireID})
-//     return questionnaire
-// }
-
-// const getQuestionnaire = async (questionnaireID) => {
-//     const questionnaire = await Questionnaire.find({ questionnaireID })
-//     const result = (({ questionnaireID, questionnaireTitle, keywords, questions: [{ qID, qtext, required, type }] }) =>
-//     ({ questionnaireID, questionnaireTitle, keywords, questions: [{ qID, qtext, required, type }] }))(questionnaire)
-//     return result
-// }
-
 const getquestionnaire = async ({ questionnaireID }) => {
     const questionnaire = await Questionnaire.findOne({ questionnaireID })
+    if (!questionnaire) throw ({statusCode: 400, message: 'No such questionnaire'})
     const { questionnaireTitle, keywords } = questionnaire
     const questions = questionnaire.questions.map(x => ({ qID: x.qID, qtext: x.qtext, required: x.required, type: x.type }))
     questions.sort((x, y) => (x.qID > y.qID) ? 1 : -1)
