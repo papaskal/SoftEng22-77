f37d975b5c3325a1cb89fc2d20b63ab5b331e216
Alexandros Papaskarlatos <el11097@mail.ntua.gr>
2023-01-17 03:35:48 +0200

    Add styles to homepage

--- frontend/react-app/src/components/QuestionnaireList.jsx
+++ frontend/react-app/src/components/QuestionnaireList.jsx
@@ -1,22 +1,66 @@
 import { Link } from 'react-router-dom'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.css'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import ListGroupItem from 'react-bootstrap/esm/ListGroupItem'
+
+// function QuestionnaireList({ questionnaires }) {
+//     return (
+//         <div>
+//             <ul>
+//                 {questionnaires.map((questionnaire) => {
+//                     return (
+//                         <li key={questionnaire.questionnaireID}>
+//                             {questionnaire.questionnaireTitle}
+//                             < Link to={`/survey/${questionnaire.questionnaireID}`} >
+//                                 <button>{questionnaire.questionnaireID}</button>
+//                             </Link>
+//                         </li>
+//                     )
+//                 })}
+//             </ul>
+//         </div>
+//     )
+// }
 
 function QuestionnaireList({ questionnaires }) {
     return (
-        <div>
-            <ul>
-                {questionnaires.map((questionnaire) => {
-                    return (
-                        <li key={questionnaire.questionnaireID}>
+        <Container className='p-3 m-3 text-center'>
+            {questionnaires.map((questionnaire) => {
+                return (
+                    <Card key={questionnaire.questionnaireID} className='p-3 m-3'>
+                        <Card.Title>
                             {questionnaire.questionnaireTitle}
-                            < Link to={`/survey/${questionnaire.questionnaireID}`} >
-                                <button>{questionnaire.questionnaireID}</button>
-                            </Link>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
+                        </Card.Title>
+                        < Link to={`/survey/${questionnaire.questionnaireID}`} >
+                            <Button variant='primary'>Take this survey</Button>
+                        </Link>
+                    </Card>
+                )
+            })}
+        </Container>
     )
 }
+
+// function QuestionnaireList({ questionnaires }) {
+//     return (
+//         <Container className='p-3 m-3'>
+//             <ListGroup>
+//                 {questionnaires.map((questionnaire) => {
+//                     return (
+//                         <ListGroup.Item key={questionnaire.questionnaireID}>
+//                             {questionnaire.questionnaireTitle}
+//                             < Link to={`/survey/${questionnaire.questionnaireID}`} >
+//                                 <button>{questionnaire.questionnaireID}</button>
+//                             </Link>
+//                         </ListGroup.Item>
+//                     )
+//                 })}
+//             </ListGroup>
+//         </Container>
+//     )
+// }
 
 export default QuestionnaireList
