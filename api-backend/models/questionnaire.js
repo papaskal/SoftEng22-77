11b41a85a0212e0f31330703867f2a8b89412e93
Alexandros Papaskarlatos <el11097@mail.ntua.gr>
2023-01-30 02:38:41 +0200

    Add questionnaire validation

--- api-backend/models/questionnaire.js
+++ api-backend/models/questionnaire.js
@@ -2,24 +2,31 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const QuestionnaireSchema = new Schema({
-    questionnaireID: String,
-    questionnaireTitle: String,
-    keywords: [String],
-    questions: [
-        {
-            qID: String,
-            qtext: String,
-            required: { type: String },
-            type: { type: String },
-            options: [
-                {
-                    optID: String,
-                    opttxt: String,
-                    nextqID: String
+    questionnaireID: { type: String, required: true, unique: true, dropDups: true },
+    questionnaireTitle: { type: String, required: true },
+    keywords: [{ type: String, required: true }],
+    questions: {
+        type: [
+            {
+                qID: { type: String, required: true, unique: true, validate: (value) => value != '-' },
+                qtext: { type: String, required: true },
+                required: { type: String, required: true },
+                type: { type: String, required: true },
+                options: {
+                    type: [
+                        {
+                            optID: { type: String, required: true },
+                            opttxt: { type: String, required: true },
+                            nextqID: { type: String, required: true }
+                        }
+                    ],
+                    validate: [(value) => value.length > 0]
                 }
-            ]
-        }
-    ]
+            }
+        ],
+        required: true,
+        validate: [(value) => value.length > 0]
+    }
 },
     { timestamps: true })
 
--- api-backend/modules/questionnaire_upd.js
+++ api-backend/modules/questionnaire_upd.js
@@ -2,11 +2,57 @@ const mongoose = require('mongoose')
 const Questionnaire = require('../models/questionnaire')
 
 
-const addquestionnaire = async (data) => {
-    const quaire = JSON.parse(data)
+const validateQuestionnaire = (quaire) => {
     const questionnaire = new Questionnaire(quaire)
-    await questionnaire.save()
-    return 200
+    if (questionnaire.validateSync()) throw ({ statusCode: 400, message: 'JSON provided has invalid schema.' })
+
+    const error = { statusCode: 400, message: 'Questionnaire provided has invalid logic.' }
+    var questions = quaire.questions.slice()
+    const seen = new Set('-')
+
+    var currID = questions[questions.length - 1].qID + "a"
+    
+    for (var i = questions.length - 1; i >= 0; i--) {
+        var q = questions[i]
+        if (currID <= q.qID) throw error
+
+        for (const option of q.options){
+            if (!seen.has(option.nextqID)) throw error
+        }
+
+        var currOpt = q.options[0]
+        for (var j = 1; j < q.options.length; j++) {
+            if (currOpt.optID >= q.options[j].optID) throw error
+            currOpt = q.options[j]
+        }
+        
+        seen.add(q.qID)
+        currID = q.qID
+    }
+
+    return
+}
+
+const addquestionnaire = async (data) => {
+    if (!data) throw ({ statusCode: 400, message: 'No file provided.' })
+
+    try {
+        var quaire = JSON.parse(data)
+    } catch (err) {
+        throw ({ statusCode: 400, message: 'File provided is not a valid JSON file.' })
+    }
+
+    validateQuestionnaire(quaire)
+
+    const questionnaire = new Questionnaire(quaire)
+   
+    try {
+        await questionnaire.save()
+    } catch (err) {
+        throw ({ statusCode: 400, message: 'A questionnaire with this questionnaireID already exists.' })
+    }
+
+    return "Questionnaire added successfully."
 }
 
 module.exports = addquestionnaire
